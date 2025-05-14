948918d565e7cfef272388f71c2da17dfadc9cf5
ACCount <ACCount12@users.noreply.github.com>
2025-05-14 17:30:20 +0300

    Makes Create vibrating table run GregTech sifter recipes

--- kubejs/server_scripts/main_server.js
+++ kubejs/server_scripts/main_server.js
@@ -10,6 +10,7 @@ ServerEvents.recipes((event) => {
   gtceuAdd(event)
   createAdd(event)
   centrifugeAdd(event)
+  vibratingAdd(event)
   wiresAdd(event)
   coilingTweak(event)
   pressurizedBasinAdd(event)
